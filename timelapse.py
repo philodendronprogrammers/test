75f4dc8772f5c9330d1e2984625bc9f9c3ce6459
philodendronprogrammers <philodendronprogrammers@gmail.com>
2023-02-13 09:33:55 -0600

    test

--- timelapse.py
+++ timelapse.py
@@ -1,17 +1,40 @@
 from picamera import PiCamera
 from os import system
-import os
 from time import sleep
+import os
+import git
 
-os.chdir('/home/pi/mnt/gdrive/plantpics')
+
 camera = PiCamera()
 camera.resolution = (1024, 768)
+repo = git.Repo("/bin/pictures")
+
+
 
 for i in range(10):
+    os.chdir('/bin/pictures')
     camera.capture('image{0:04d}.jpg'.format(i))
-    sleep(10800)
-    print("captured")
-            
-system('convert -delay 10 -loop 0 image*.jpg animation.gif')
+    #sleep(10800)
+    system('convert -delay 5 -loop 0 image*.jpg animation.gif')
+    repo.git.add(A=True)
+    repo.index.commit("test")
+    origin = repo.remote(name='origin')
+    origin.push(username='philodendronprogrammers', password='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')
+
+    
+    
+
+   
+    print("captured 1")
 
 camera.close()
+
+
+
+   # os.chdir('/bin/pictures')
+
+            
+
+
+
+
